4816c344e36cc5757e87e2e59bc32e1f143d58c7
Chamith Kodikara <chamith@ceylonit.com>
2019-06-11 12:15:15 +0530

    -sample endpoint updated

--- src/main/java/com/helixz/docker/demo/controller/DockerDemoController.java
+++ src/main/java/com/helixz/docker/demo/controller/DockerDemoController.java
@@ -1,4 +1,4 @@
-package com.helixz.docker.demo;
+package com.helixz.docker.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
